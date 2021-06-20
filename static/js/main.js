6f564cae9721dd67decda11361e6e73447dbeda3
mercy maloba <your_email_address@example.com>
2021-06-20 23:58:51 +0300

    add submit

--- static/js/main.js
+++ static/js/main.js
@@ -17,3 +17,15 @@ $(document).ready(function(){
         $(this).children(".bac").fadeToggle(1000, "linear");
     })
 })
+$(document).ready(function(){
+    $(".btn").click(function(){
+        var name1= $("#namee").val();
+        var email= $("#emaile").val();
+        var textA= $("#comment").val();
+        if (name1 == '' || email == '' || textA == '') {
+            alert('Please make sure you have filled in the form correctly!');
+        } else {
+            alert(' Hi ' + name1 + ' We have received your message. Thank you for reaching out to us.');
+        }
+    })   
+})
