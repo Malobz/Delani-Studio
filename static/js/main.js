13c68e5a5c459c21a791ddeb62ea0ba901234d41
mercy maloba <your_email_address@example.com>
2021-06-20 21:37:19 +0300

    add toggle method to classes

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".what1,.d-description").click(function(){
+        $(".what1").toggle();
+        $(".d-description").toggle();
+    })
+    $(".what2, .dev-des").click(function(){
+        $(".what2").toggle();
+        $(".dev-des").toggle();
+    })
+    $(".what3, .pro-des").click(function(){
+        $(".what3").slideDown().toggle();
+        $(".pro-des").slideUp().toggle();
+    })
+})
