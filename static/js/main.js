9d32767628eb1a692229f8ec38baafe9ecde310c
mercy maloba <your_email_address@example.com>
2021-06-22 02:17:20 +0300

    add hover function

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,33 @@
-$(document).ready(function(){
-    $(".what1,.d-description").click(function(){
-        $(".what1").toggle();
+$(document).ready(function() {
+    $(".design").click(function() {
         $(".d-description").toggle();
-    })
-    $(".what2, .dev-des").click(function(){
-        $(".what2").toggle();
+        $(".desg").toggle();
+    });
+
+    $(".dev").click(function() {
         $(".dev-des").toggle();
-    })
-    $(".what3, .pro-des").click(function(){
-        $(".what3").slideDown().toggle();
-        $(".pro-des").slideUp().toggle();
+        $(".devl").toggle();
+    });
+    $(".product").click(function() {
+        $(".pro-des").toggle();
+        $(".prd").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".imge").hover(function() {
+        $(this).children(".imge1").fadeToggle(1000, "linear");
     })
 })
-$(document).ready(function(){
-    $(".hov").hover(function(){
-        $(this).children(".bac").fadeToggle(1000, "linear");
-    })
-})
-$(document).ready(function(){
-    $(".btn").click(function(){
-        var name1= $("#namee").val();
-        var email= $("#emaile").val();
-        var textA= $("#comment").val();
+
+$(document).ready(function() {
+    $(".btn").click(function() {
+        var name1 = $("#namee").val();
+        var email = $("#emaile").val();
+        var textA = $("#comment").val();
         if (name1 == '' || email == '' || textA == '') {
             alert('Please make sure you have filled in the form correctly!');
         } else {
             alert(' Hi ' + name1 + ' We have received your message. Thank you for reaching out to us.');
         }
-    })   
+    })
 })
